90aed51c86c4628e097f400dfbf80b4c20953867
KristalyJimmy <kristalyjimmy@yahoo.com>
2023-03-01 15:31:40 +0100

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,13 @@
+export const robots = [
+    {id: 1, name: 'Leanne Graham', username: 'Bret', email: 'sincerebret@april.cz'},
+    {id: 2, name: 'Ervin Howell', username: 'Antonette', email: 'shanna@melissa.tv'},
+    {id: 3, name: 'Clementine Bauch', username: 'Samantha', email: 'nathan@yesenia.net'},
+    {id: 4, name: 'Patricia Lebsack', username: 'Karianne', email: 'julianne.oconner@kory.org'},
+    {id: 5, name: 'Chelsey Dietrich', username: 'Kamren', email: 'lucio_hettinger@annie.ca'},
+    {id: 6, name: 'Mrs. Dennis Schulist', username: 'Leopoldo_Corkery', email: 'karley_dach@jasper.info'},
+    {id: 7, name: 'Kurtis Weissnat', username: 'Elwyn.Skiles', email: 'telly.hoeger@billy.biz'},
+    {id: 8, name: 'Nicholas Runolfsdottir V', username: 'Maxime_Nienow', email: 'sherwood@rosamond.me'},
+    {id: 9, name: 'Glenna Reichert', username: 'Delphine', email: 'chaim_mcdermott@dana.io'},
+    {id: 10, name: 'Clementina DuBuque', username: 'Moriah.Stanton', email: 'rebecca_okeefe@kristin.biz'}
+  ];
+  
