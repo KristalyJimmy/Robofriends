4cc230545e606f8d729052be22421ae25cb7c127
KristalyJimmy <kristalyjimmy@yahoo.com>
2023-03-02 15:44:08 +0100

    Updates

---
+++ precache-manifest.fa011cb435da3999df6d3af0bd65fa99.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "42e68faf2abfbdf51d12",
+    "url": "/Robofriends/static/css/main.b9b7c486.chunk.css"
+  },
+  {
+    "revision": "42e68faf2abfbdf51d12",
+    "url": "/Robofriends/static/js/main.42e68faf.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/Robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "1d39b36c34ecb64d715d",
+    "url": "/Robofriends/static/css/2.d2bfe6a3.chunk.css"
+  },
+  {
+    "revision": "1d39b36c34ecb64d715d",
+    "url": "/Robofriends/static/js/2.1d39b36c.chunk.js"
+  },
+  {
+    "revision": "3fb263664dd9048408a3",
+    "url": "/Robofriends/static/js/runtime~main.3fb26366.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "705b75f3fb15ae0e04162e80efd13ebf",
+    "url": "/Robofriends/index.html"
+  }
+];
